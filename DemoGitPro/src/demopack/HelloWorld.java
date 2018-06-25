e9e4413d3e95804a42a813d63712bd88da2ba5f3
sony <sony@sony-PC>
2018-06-25 23:59:46 +0530

    commiting first time
    Change-Id: Ib1417d9af0bca9bb5a54fa8e8d30540a303c42d9
    Signed-off-by: sony <sony@sony-PC>

---
+++ DemoGitPro/src/demopack/HelloWorld.java
@@ -0,0 +1,10 @@
+package demopack;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Helloooooooooo Javvvvvvvvvvva");
+	}
+
+}
